2aafa2952f76ea1a1781e3a3ea3245fd3f8a3d80
boydeveloper <wilfriedakpe3@gmail.com>
2022-03-15 10:35:51 +0100

    cards component setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+const cardImgs = [
+  { src: "/img/helmet-1.png" },
+  { src: "/img/potion-1.png" },
+  { src: "/img/ring-1.png" },
+  { src: "/img/scroll-1.png" },
+  { src: "/img/shield-1.png" },
+  { src: "/img/sword-1.png" },
+];
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  const shuffleCards = () => {
+    let cardsToshuffle = [...cardImgs, ...cardImgs]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(cardsToshuffle);
+    setTurns(0);
+  };
+  console.log(cards, turns);
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
+      <div className="card-grid">
+        {cards.map((card) => {
+          return (
+            <div className="card" key={card.id}>
+              <div>
+                <img src={card.src} alt="card front" className="front" />
+                <img src="/img/cover.png" alt="card back" className="back" />
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
 
-export default App
+export default App;
